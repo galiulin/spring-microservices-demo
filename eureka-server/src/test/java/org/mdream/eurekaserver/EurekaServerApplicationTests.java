392e19467b3edeb3bda51d78b5377a2c9e79c38b
Galiulin Arthur <Galiulin.Arthur@gmail.com>
2022-10-25 22:23:37 +0300

    init module eureka-server

---
+++ eureka-server/src/test/java/org/mdream/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mdream.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
