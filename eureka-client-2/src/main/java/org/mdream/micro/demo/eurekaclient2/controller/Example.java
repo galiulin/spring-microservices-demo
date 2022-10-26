205045c30087fb2de44aeb0d20e64917ace79c5b
Galiulin Arthur <Galiulin.Arthur@gmail.com>
2022-10-27 00:51:08 +0300

    example of creating multiple instances of the same service

--- eureka-client-2/src/main/java/org/mdream/micro/demo/eurekaclient2/controller/Example.java
+++ eureka-client-2/src/main/java/org/mdream/micro/demo/eurekaclient2/controller/Example.java
@@ -1,5 +1,6 @@
 package org.mdream.micro.demo.eurekaclient2.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/example")
 public class Example {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/first")
     public String take() {
-        return "hello";
+        return instanceId;
     }
 }
